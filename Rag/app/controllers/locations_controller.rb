c2d4ca69d5357526fc73fa9f1a97e8fa755d6152
wulfman97 <calebschade97@gmail.com>
2015-09-12 11:50:55 -0600

    changed locations controller

--- Rag/app/controllers/locations_controller.rb
+++ Rag/app/controllers/locations_controller.rb
@@ -1,10 +1,8 @@
 class LocationsController < ApplicationController
+  before_action :find_location, :only [:edit, :show, :update, :destroy]
+  
   def index
     @locations = Location.all
-  end
-
-  def show
-    @location = Location.find(params[:id])
   end
 
   def new
@@ -21,12 +19,36 @@ class LocationsController < ApplicationController
   end
 
   def edit
-    @location = Location.find(params[:id])
   end
 
+  def update
+    if @location.update(location_params)
+      redirect_to location_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def random_location
+    @random_location = @locations.sample
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to location_path
+  end
+    
   private
 
   def location_params
     params.require(:location).permit(:address, :latitude, :longitude)
   end
+
+  def find_location
+    @location = Location.find(params[:id])
+  end
+
 end
