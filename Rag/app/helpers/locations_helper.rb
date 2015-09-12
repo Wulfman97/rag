61c1576f83cdf787fa7d80d9104411c886882cf4
wulfman97 <calebschade97@gmail.com>
2015-09-12 16:20:15 -0600

    resolve merge conflict

--- Rag/app/helpers/locations_helper.rb
+++ Rag/app/helpers/locations_helper.rb
@@ -1,6 +1,9 @@
 module LocationsHelper
-  def location_map_url(location)
-    "http://maps.google.com/maps/?q=" + location.latitude.to_s + "," + @location.longitude.to_s
+  def location_map_url
+    "http://maps.google.com/maps/?q=" + @location.latitude.to_s + "," + @location.longitude.to_s
+  end
+  def random_location_map_url
+    "http://maps.google.com/maps/?q=" + @random_location.latitude.to_s + "," + @random_location.longitude.to_s
   end
 
 end
