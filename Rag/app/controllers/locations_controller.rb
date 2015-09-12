cac84e8acf263499c0fd632a1a41147955f2c9f7
bondparkerbond <parkerbond@gmail.com>
2015-09-12 10:45:13 -0600

    add locations crud methods

--- Rag/app/controllers/locations_controller.rb
+++ Rag/app/controllers/locations_controller.rb
@@ -1,13 +1,32 @@
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:address, :latitude, :longitude)
   end
 end
