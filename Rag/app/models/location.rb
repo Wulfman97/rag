4dcc2a99aeb813b0e6564fd163b06d2340d857ac
wulfman97 <calebschade97@gmail.com>
2015-09-12 13:30:04 -0600

    fixed locations lat/long

--- Rag/app/models/location.rb
+++ Rag/app/models/location.rb
@@ -1,8 +1,6 @@
 class Location < ActiveRecord::Base
-
-  #attr_accessible :address, :latitude, :longitude
-  #geocoded_by :address
-  #after_validation :geocode
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
   
   has_many :comments
 
